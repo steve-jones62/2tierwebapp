54bfb5f441af5e6383600cc35a5a3d176f86a36b
steve-jones62 <steve@kupjones.com>
2025-01-02 10:02:50 -0600

    fix imports

--- app4/frontendapp.py
+++ app4/frontendapp.py
@@ -1,9 +1,23 @@
+#! /usr/bin/env python3
 # frontend_server.py
 import os
 from flask import Flask, render_template, send_from_directory
 import requests
+import socket
+import json
+from datetime import datetime
+
+
 
 app = Flask(__name__)
+HOSTNAME = socket.gethostname()
+IP_ADDR = socket.gethostbyname(HOSTNAME)
+NAMESPACE = "gitops-webby" if "gitops" in HOSTNAME else "webby"
+PORT = "30999" if "gitops" in HOSTNAME else "5001"
+# BASE_URL = f"http://ocp01-hpe02.pedemo.ignw.io:{PORT}"
+# Changed with new RHOS Route - make sure it is created
+BASE_URL = "https://webby-webby.apps.ocp-hpe02.pedemo.ignw.io/" if "webby" in HOSTNAME else "https://127.0.0.1/"
+
 
 # This grabs the file 'index.html' out of the TEMPLATES subdir and serves it.
 @app.route('/')
