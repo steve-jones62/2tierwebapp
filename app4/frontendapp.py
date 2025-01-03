948c925358a776556e9b2b8b14f194d458cd84d7
steve-jones62 <steve@kupjones.com>
2025-01-03 15:27:30 -0600

    more

--- app4/frontendapp.py
+++ app4/frontendapp.py
@@ -41,7 +41,9 @@ def query_backend():
     backend_url = 'http://127.0.0.1:5001/info'
     try:
         response = requests.get(backend_url)
+        print response.json()
         return response.json()
+        
     except requests.exceptions.RequestException as e:
         return {"error": str(e)}
 
